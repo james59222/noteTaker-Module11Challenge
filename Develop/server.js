15e6a94d01da7735aed4d22ccdc6af980dca92c6
James Pierce <james59222@gmail.com>
2023-04-20 19:04:01 -0500

    worked on server.js

--- Develop/server.js
+++ Develop/server.js
@@ -14,14 +14,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // GET Route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+// GET /notes should return the notes.html file.
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// GET * should return the index.html file.
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+//function to create new notes 
+
+
+//create new notes need to include uuid.
+app.post('/api/notes', (req, res) => {
+console.info(`${req.method} request received to add a note`);
+const newNote = createNewNote(req.body, Notes);
+res.json(newNote);
+});
+
+
+//read notes
+
+//delete notes for extra credit.
+
 
 
 app.listen(PORT, () =>
